1d6a01f563f22a1452a115110cce1227cfba636f
james <emperor2g@gmail.com>
2024-11-17 15:16:27 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+rl.question("please enter your name: ", (name) => {
+  console.log("You entered: " + name);
+  rl.close();
+})
