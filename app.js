60d88e7e4d0e189648ba20b96794542351a47bb1
james <emperor2g@gmail.com>
2024-11-17 17:00:46 +0100

    reading and writing into a text file

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const readline = require('readline');
+const fs = require('fs')  //fs meaning file system module
+/* LECTURE 4: CODE EXAMPLE
+*********************************
+READING INPUT & WRITING OUTPUT
+************************************************************
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -9,3 +14,20 @@ rl.question("please enter your name: ", (name) => {
   console.log("You entered: " + name);
   rl.close();
 })
+
+
+rl.on('close', ()=>{
+  console.log('Interface closed! Goodbye!')
+  process.exit(0);
+})*/
+
+/* LECTURE 5: CODE EXAMPLE***************** 
+READING & WRITING TO A FILE
+***********************************************/
+let textIn = fs.readFileSync('./Files/input.txt', 'utf-8');
+console.log(textIn);
+
+let content = `Data read from input.txt: ${textIn}. \ndate created ${new Date()}`
+fs.writeFileSync('./Files/output.txt', content)
+
+
