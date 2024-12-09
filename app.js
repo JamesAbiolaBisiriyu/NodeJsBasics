2e2cca3491ddc974179a89e833ba6d04dddc1499
james <emperor2g@gmail.com>
2024-12-09 06:55:06 +0100

    creating a reusable function

--- app.js
+++ app.js
@@ -1,55 +1,12 @@
+// CORE MODULES OF NODE-JS
 const readline = require('readline');
 const fs = require('fs')  //fs meaning file system module
 const http = require('http');
 const url = require('url'); //used for routing
-/* LECTURE 4: CODE EXAMPLE
-*********************************
-READING INPUT & WRITING OUTPUT
-************************************************************
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const replaceHtml = require('./Modules/replaceHtml')
+// USER DEFINED MODULES OR CUSTOM MODULES
 
-
-rl.question("please enter your name: ", (name) => {
-  console.log("You entered: " + name);
-  rl.close();
-})
-
-
-rl.on('close', ()=>{
-  console.log('Interface closed! Goodbye!')
-  process.exit(0);
-})*/
-
-/* LECTURE 5: CODE EXAMPLE***************** 
-READING & WRITING TO A FILE
-***********************************************/
-/*let textIn = fs.readFileSync('./Files/input.txt', 'utf-8');
-console.log(textIn);
-
-let content = `Data read from input.txt: ${textIn}. \ndate created ${new Date()}`
-fs.writeFileSync('./Files/output.txt', content)*/
-
-/*LECTURE 7: CODE EXAMPLE *****************
-READING & WRITING TO FILES ASYNCHRONOUSLY
-**********************************************/
-/*fs.readFile('./Files/start.txt', 'utf-8', (error1, data1)=>{
-  console.log(data1);  
-  fs.readFile(`./Files/${data1}.txt`, 'utf-8', (error2, data2)=> {
-    console.log(data2);
-    fs.readFile('./Files/append.txt', 'utf-8', (error3, data3)=> {
-      console.log(data3);
-      fs.writeFile('./Files/output.txt', `${data2}\n\n${data3}\n\ndate created ${new Date()}`, ()=>{
-        console.log('file written Successfully')
-      });
-      
-    })
-    
-  })
-})
-console.log('Reading File .............');*/
+// THIRD PARTY MODULE
 
 /*LECTURE 8: CODE EXAMPLE ***********************
 CREATING A SIMPLE WEB SERVER
@@ -58,27 +15,6 @@ const html = fs.readFileSync('./Template/index.html', 'utf-8');
 let products = JSON.parse(fs.readFileSync('./Data/products.json', 'utf-8'));
 let productListHtml = fs.readFileSync('./Template/product-list.html', 'utf-8');
 let productDetailHtml = fs.readFileSync('./Template/product-details.html', 'utf-8');
-
-
-
-
-function replaceHtml (template, product) {
-  let output = template.replace('{{%IMAGE%}}', product.productImage);
-  output = output.replace('{{%NAME%}}', product.name);
-  output = output.replace('{{%MODELNAME%}}', product. modeName);
-  output = output.replace('{{%MODELNO%}}', product.modelNumber);
-  output = output.replace('{{%SIZE%}}', product.size);
-  output = output.replace('{{%CAMERA%}}', product.camera);
-  output = output.replace('{{%PRICE%}}', product.price);
-  output = output.replace('{{%COLOR%}}', product.color);
-  output = output.replace('{{%ID%}}', product.id);
-  output = output.replace('{{%ROM%}}', product.ROM);
-  output = output.replace('{{%DESC%}}', product.Description);
-
-
-
-  return output;
-}
 
 // STEP 1: CREATE A SERVER
 const server = http.createServer((request, response)=>{
@@ -92,7 +28,7 @@ const server = http.createServer((request, response)=>{
       'Content-Type': 'text/html',
       'my-header' : 'hello world'
     });
-    response.end(html.replace('{{%CONTENT%}}', productListHtml))
+    response.end(html.replace('{{%CONTENT%}}', 'YOU ARE IN HOMEPAGE'))
   } else if (path.toLocaleLowerCase() === '/about') {
     response.writeHead(200, {
       'Content-Type': 'text/html',
@@ -117,7 +53,7 @@ const server = http.createServer((request, response)=>{
     // console.log(productHtmlArray.join(','));
     }else{
       let prod = products[query.id]
-      let productDetailResponseHtml = replaceHtml(productDetailHtml, prod )
+      let productDetailResponseHtml = replaceHtml (productDetailHtml, prod )
       response.end(html.replace('{{%CONTENT%}}', productDetailResponseHtml))
     }
   } 
@@ -127,15 +63,12 @@ const server = http.createServer((request, response)=>{
       'my-header' : 'hello world'
     });
     response.end(html.replace('{{%CONTENT%}}', 'PAGE NOT FOUND, ERROR 404'))
-  }
-  
-  
+  } 
 })
 
 //STEP 2: START THE SERVER
 server.listen(8000, '127.0.0.1', ()=>{
-  console.log('Server has started!');
-  
+  console.log('Server has started!');  
 })
 
 
